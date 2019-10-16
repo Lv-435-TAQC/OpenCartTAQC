f4abbaa0976ff8180e22c6d376ae920c51fbf5ee
nhardzhalo <ngardzhalo@gmail.com>
2019-10-16 14:08:51 +0300

    fix

--- src/main/java/locators/WishListLocators.java
+++ src/main/java/locators/WishListLocators.java
@@ -1,6 +1,6 @@
 package locators;
 
 public class WishListLocators {
-    public static final String WISH_LIST_TABLE = "(\"//*[@id=\\\"content\\\"]/div[1]/table/tbody";
+    public static final String WISH_LIST_TABLE = "//*[@id=\"content\"]/div[1]/table/tbody";
 
 }
