8b55bb27ea680eda27b6dcd62e00dea1a867ffae
Liubomyr Karp <lyubomur.karp@gmail.com>
2019-10-16 08:59:24 +0300

    searchPageObject

--- src/main/java/locators/SearchLocators.java
+++ src/main/java/locators/SearchLocators.java
@@ -1,4 +1,8 @@
 package locators;
 
 public class SearchLocators {
+    public static final String INPUTE_SEARCH_TEXT_XPATH = "//*[@id=\"input-search\"]";
+    public static final String CATEGORIES_BUTTON_XPATH = "//*[@id=\"content\"]/div/div[2]/select";
+    public static final String SEARCH_IN_SUBCATEGORIES_CHECKBOX = "//*[@id=\"content\"]/div[1]/div[3]/label/input";
+    public static final String SEARCH_BUTTON_XPATH = "//*[@id=\"button-search\"]";
 }
