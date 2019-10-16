a27f0d1ff0995285b117c6eeba9b5ab259f0f7d1
andr0306 <andrijrubchuk@gmail.com>
2019-10-16 15:27:36 +0300

    almost final version

--- src/main/java/pageobjects/HeaderPageObject.java
+++ src/main/java/pageobjects/HeaderPageObject.java
@@ -1,6 +1,7 @@
 package pageobjects;
 
 import locators.HeaderLocators;
+import locators.MenuLocators;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,9 +15,8 @@ import java.util.List;
 import static utils.commonconstants.Constants.*;
 
 public class HeaderPageObject extends BasePageObject {
-    WebDriverWait wait = new WebDriverWait(driver,10);
+    WebDriverWait wait = new WebDriverWait(driver, 10);
     List<WebElement> listOfCurrency;
-    List<WebElement> listOfMyAccountButtons;
     private Button wishPageButton;
     private Button itemsPageButton;
     private Label yourStore;
@@ -24,71 +24,88 @@ public class HeaderPageObject extends BasePageObject {
     private Button loginPageButton;
     private Button registrationPageButton;
     private Button shoppingCartButton;
+    private Button checkoutButton;
     private Button currencyListButton;
-    private String itemButtonText;
-    private Label firstElementOfTape;
-    private Label secondElementOfTape;
-    private
+    private String buttonText;
+
+
     public HeaderPageObject(WebDriver driver) {
         super(driver);
     }
 
-    private List<WebElement>createListOfCurrency(String xpath){
-       listOfCurrency =  driver.findElements(By.xpath(xpath));
+    private List<WebElement> createListOfCurrency(String xpath) {
+        listOfCurrency = driver.findElements(By.xpath(xpath));
         return listOfCurrency;
     }
-    private String getTextOfButton(){
-        return
-    }
 
-    public HomePageObject goToHomePage(){
+    public HomePageObject goToHomePage() {
         yourStore = new LinkedLabel(driver, HeaderLocators.YOUR_STORE_BUTTON_LOC).click();
         return new HomePageObject(driver);
     }
-    public ItemPageObject goToItemsPage(){
-        itemsPageButton = new ImageTextButton(driver,HeaderLocators.ITEMS_PAGE_BUTTON_LOC).click();
+
+    public ItemPageObject goToItemsPage() {
+        itemsPageButton = new ImageTextButton(driver, HeaderLocators.ITEMS_PAGE_BUTTON_LOC).click();
         return new ItemPageObject(driver);
     }
-    public WishListPageObject goToWishList(){
-        wishPageButton = new ImageTextButton(driver,HeaderLocators.WISH_LIST_PAGE_BUTTON_LOC).click();
+
+    public WishListPageObject goToWishList() {
+        wishPageButton = new ImageTextButton(driver, HeaderLocators.WISH_LIST_PAGE_BUTTON_LOC).click();
         return new WishListPageObject(driver);
     }
-    public LoginPageObject goToLoginPage(){
-        myAccountButton = new ImageTextButton(driver,HeaderLocators.MY_ACCOUNT_BUTTON_LOC).click();
+
+    public LoginPageObject goToLoginPage() {
+        myAccountButton = new ImageTextButton(driver, HeaderLocators.MY_ACCOUNT_BUTTON_LOC).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath(HeaderLocators.LOGIN_PAGE_BUTTON_LOC)));
-        loginPageButton = new TextButton(driver,HeaderLocators.LOGIN_PAGE_BUTTON_LOC).click();
+        loginPageButton = new TextButton(driver, HeaderLocators.LOGIN_PAGE_BUTTON_LOC).click();
         return new LoginPageObject(driver);
     }
-    public RegistrationPageObject goToRegistrationPage(){
-        myAccountButton = new ImageTextButton(driver,HeaderLocators.MY_ACCOUNT_BUTTON_LOC).click();
+
+    public RegistrationPageObject goToRegistrationPage() {
+        myAccountButton = new ImageTextButton(driver, HeaderLocators.MY_ACCOUNT_BUTTON_LOC).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath(HeaderLocators.REGISTRATION_PAGE_BUTTON_LOC)));
-        registrationPageButton = new TextButton(driver,HeaderLocators.REGISTRATION_PAGE_BUTTON_LOC).click();
+        registrationPageButton = new TextButton(driver, HeaderLocators.REGISTRATION_PAGE_BUTTON_LOC).click();
         return new RegistrationPageObject(driver);
     }
-    public ShoppingCartPageObject goToShoppingCartPage(){
-        shoppingCartButton = new ImageTextButton(driver,HeaderLocators.SHOPPING_CART_PAGE_BUTTON_LOC).click();
+
+    public ShoppingCartPageObject goToShoppingCartPage() {
+        shoppingCartButton = new ImageTextButton(driver, HeaderLocators.SHOPPING_CART_PAGE_BUTTON_LOC).click();
         return new ShoppingCartPageObject(driver);
     }
-    public HeaderPageObject chooseDollarCurrency(){
-        currencyListButton = new ImageTextButton(driver,HeaderLocators.CURRENCY_BUTTON_LOC).click();
+    public ShoppingCartPageObject goToCheckoutCartPage(){
+        checkoutButton = new ImageTextButton(driver,HeaderLocators.CHECKOUT_PAGE_BUTTON_LOC).click();
+        return new ShoppingCartPageObject(driver);
+    }
+
+    public HeaderPageObject chooseDollarCurrency() {
+        currencyListButton = new ImageTextButton(driver, HeaderLocators.CURRENCY_BUTTON_LOC).click();
         this.createListOfCurrency(HeaderLocators.LIST_OF_CURRENCY_LOC).get(TWO).click();
         return new HeaderPageObject(driver);
     }
-    public HeaderPageObject chooseEuroCurrency(){
-        currencyListButton = new ImageTextButton(driver,HeaderLocators.CURRENCY_BUTTON_LOC).click();
+
+    public HeaderPageObject chooseEuroCurrency() {
+        currencyListButton = new ImageTextButton(driver, HeaderLocators.CURRENCY_BUTTON_LOC).click();
         this.createListOfCurrency(HeaderLocators.LIST_OF_CURRENCY_LOC).get(NULL).click();
         return new HeaderPageObject(driver);
     }
-    public HeaderPageObject choosePoundCurrency(){
-        currencyListButton = new ImageTextButton(driver,HeaderLocators.CURRENCY_BUTTON_LOC).click();
+
+    public HeaderPageObject choosePoundCurrency() {
+        currencyListButton = new ImageTextButton(driver, HeaderLocators.CURRENCY_BUTTON_LOC).click();
         this.createListOfCurrency(HeaderLocators.LIST_OF_CURRENCY_LOC).get(ONE).click();
         return new HeaderPageObject(driver);
     }
-    public String getItemsButtonText(){
-        itemButtonText = new TextButton(driver,HeaderLocators.ITEMS_PAGE_BUTTON_LOC).getText();
-        return itemButtonText;
+
+    public String getTextFromFirstTape() {
+        buttonText = new LinkedLabel(driver, HeaderLocators.FIRST_ELEMENT_OF_TAPE).getText();
+        return buttonText;
     }
-    public String getText(){
-        return
+
+    public String getTextFromSecondTape() {
+        buttonText = new LinkedLabel(driver, HeaderLocators.SECOND_ELEMENT_OF_TAPE).getText();
+        return buttonText;
+    }
+
+    public String getTextFromItems() {
+        buttonText = new LinkedLabel(driver, HeaderLocators.ITEMS_PAGE_BUTTON_LOC).getText();
+        return buttonText;
     }
 }
--- src/test/javatest/pageobjectstest/HeaderPageObjectTest.java
+++ src/test/javatest/pageobjectstest/HeaderPageObjectTest.java
@@ -10,48 +10,89 @@ import pageobjects.HeaderPageObject;
 import pageobjects.MenuPageObject;
 
 import java.util.concurrent.TimeUnit;
+
 import static org.testng.Assert.*;
 
 public class HeaderPageObjectTest {
-        WebDriver driver;
-        HeaderPageObject header;
+    WebDriver driver;
+    HeaderPageObject header;
 
 
-        @BeforeClass
-        public void setUp() {
-            System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
-            driver = new FirefoxDriver();
-        }
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
 
-        @BeforeMethod
-        public void getHome() {
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            driver.get("http://localhost/shop");
-            header = new HeaderPageObject(driver);
+    @BeforeMethod
+    public void getHome() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://localhost/shop");
+        header = new HeaderPageObject(driver);
 
-        }
+    }
 
-        @AfterClass
-        public void tearDown() {
-            driver.close();
-        }
-        @Test
-        public void euroCurrencyTest(){
-            String actual = header.chooseEuroCurrency().getItemsButtonText();
-            String expected = "0 item(s) - 0.00€";
-            assertEquals(expected,actual);
-        }
-        @Test
-         public void dollarCurrencyTest(){
-        String actual = header.chooseDollarCurrency().getItemsButtonText();
+    @AfterClass
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void euroCurrencyTest() {
+        String actual = header.chooseEuroCurrency().getTextFromItems();
+        String expected = "0 item(s) - 0.00€";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void dollarCurrencyTest() {
+        String actual = header.chooseDollarCurrency().getTextFromItems();
         String expected = "0 item(s) - $0.00";
-        assertEquals(expected,actual);
-        }
-        @Test
-         public void poundCurrencyTest(){
-        String actual = header.choosePoundCurrency().getItemsButtonText();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void poundCurrencyTest() {
+        String actual = header.choosePoundCurrency().getTextFromItems();
         String expected = "0 item(s) - £0.00";
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
+
+    @Test
+    public void testLoginPage() {
+        header.goToLoginPage();
+        String actual = header.getTextFromSecondTape();
+        String expected = "Login";
+        assertEquals(actual, expected);
     }
+
+    @Test
+    public void testRegistrationPage() {
+        header.goToRegistrationPage();
+        String actual = header.getTextFromSecondTape();
+        String expected = "Register";
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testWishListPage(){
+        header.goToWishList();
+        String actual = header.getTextFromSecondTape();
+        String expected = "Login";
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void testShoppingCartPage(){
+    header.goToShoppingCartPage();
+    String actual = header.getTextFromFirstTape();
+    String expected = "Shopping Cart";
+    assertEquals(actual,expected);
+    }
+    @Test
+    public void testCheckoutPage(){
+        header.goToCheckoutCartPage();
+        String actual = header.getTextFromFirstTape();
+        String expected = "Shopping Cart";
+        assertEquals(actual,expected);
+    }
+}
 
