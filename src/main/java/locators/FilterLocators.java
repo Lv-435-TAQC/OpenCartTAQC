13526ba553dbcc63cb44551293cb984701a49ec5
ollehhh <oleh.zarichnyi@gmail.com>
2019-10-16 14:04:09 +0300

    Chacnge 2 locators

--- src/main/java/locators/FilterLocators.java
+++ src/main/java/locators/FilterLocators.java
@@ -4,9 +4,9 @@ public class FilterLocators {
     public static final String SHOW_LIST_BUTTON = "//*[@id=\"list-view\"]";
     public static final String SHOW_GRID_BUTTON = "//*[@id=\"grid-view\"]";
     public static final String PRODUCT_COMPARE_LABEL = "//*[@id=\"compare-total\"]";
-    public static final String SORT_BY_LABEL = "/html/body/div[2]/div/div/div[3]/div[3]/div/label";
+    public static final String SORT_BY_LABEL = "//label[@class = 'input-group-addon' and @for = 'input-sort']";
     public static final String SORT_BY_SELECTOR = "//*[@id=\"input-sort\"]";
     public static final String SHOW_SELECTOR = "//*[@id=\"input-limit\"]";
-    public static final String SHOW_LABEL = "/html/body/div[2]/div/div/div[3]/div[4]/div/label";
+    public static final String SHOW_LABEL = "//label[@class = 'input-group-addon' and @for = 'input-limit']";
 
 }
