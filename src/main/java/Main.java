0fff31bc5bd9bb93ee5934e46ddffe12e3e92c74
ollehhh <oleh.zarichnyi@gmail.com>
2019-10-17 14:51:16 +0300

    delete test code from main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,5 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import pageobjects.CategoryPageObject;
-
 public class Main {
     public static void main(String[] args) {
-        WebDriver driver;
-        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
-        driver = new FirefoxDriver();
-        driver.get("http://192.168.92.128/opencart/index.php?route=product/category&path=20");
-        CategoryPageObject pg = new CategoryPageObject(driver);
-        pg.clickToImageByNumberOfProduct(1);
 
     }
 
