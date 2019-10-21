1732cb367c044e908dd5ba8f65b0c70f7a86da80
andr0306 <andrijrubchuk@gmail.com>
2019-10-21 15:39:25 +0300

    add some methods

--- src/main/java/pageobjects/NavigationPageObject.java
+++ src/main/java/pageobjects/NavigationPageObject.java
@@ -1,12 +1,24 @@
 package pageobjects;
 
+import locators.AddNewProductsLocators;
+import locators.AdminProductsPageLocators;
+import locators.NavigationLocators;
 import org.openqa.selenium.WebDriver;
+import pageelements.Button;
+import pageelements.TextButton;
 import pageobjects.BasePageObject;
 
 public class NavigationPageObject extends BasePageObject {
 
+    private Button products;
 
     public NavigationPageObject(WebDriver driver) {
         super(driver);
     }
+
+    public AdminProductsList goToProducts(){
+        products = new TextButton(driver, NavigationLocators.PRODUCTS_PAGE_BUTTON_LOC).click();
+        return new AdminProductsList(driver);
+    }
+
 }
