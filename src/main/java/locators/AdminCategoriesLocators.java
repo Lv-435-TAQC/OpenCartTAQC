a53f5f1da52888a1951a314cba4237a3395ca76e
Liubomyr Karp <lyubomur.karp@gmail.com>
2019-10-22 13:21:11 +0300

    addLocators

---
+++ src/main/java/locators/AdminCategoriesLocators.java
@@ -0,0 +1,10 @@
+package locators;
+
+public class AdminCategoriesLocators {
+    public static final String ADD_NEW_CATEGORIES_BUTTON_LOC = "//*[@id=\"content\"]/div[1]/div/div/a[1]";
+    public static final String DELETE_CATEGORIES_BUTTON_LOC = "//*[@id=\"content\"]/div[1]/div/div/button";
+    public static final String REBUILD_CATEGORIES_BUTTON_LOC = "//*[@id=\"content\"]/div[1]/div/div/a[2]";
+    public static final String CHOOSE_CATEGORIES_BUTTON_LOC = "//*[@id=\"form-category\"]/div/table/tbody/tr[13]/td[1]/input";
+    public static final String EDIT_CATEGORIES_BUTTON_LOC = "//*[@id=\"form-category\"]/div/table/tbody/tr[13]/td[4]/a";
+
+}
