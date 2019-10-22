c261c45dc4af6ddc76016687aa1eee12df6a14d2
Orysia Benko <orysita.lviv@gmail.com>
2019-10-22 15:32:01 +0300

    add admin login pageobject

---
+++ src/main/java/locators/AdminLoginLocators.java
@@ -0,0 +1,11 @@
+package locators;
+
+public class AdminLoginLocators {
+    public static final String OPEN_CARD_LOGO = "//*[@id=\"header-logo\"]";
+    public static final String INPUT_ADMIN_EMAIL_FIELD = "//*[@id=\"input-username\"]";
+    public static final String INPUT_PASSWORD_FIELD = "//*[@id=\"input-password\"]";
+    public static final String LOGIN_BUTTON = "//*[@id=\"content\"]/div/div/div/div/div[2]/form/div[3]/button";
+    public static final String GO_TO_REAL_OPENCART = "//*[@id=\"footer\"]/a";
+    public static final String FOGOTTEN_PASSWORD_ADMIN = "//*[@id=\"content\"]/div/div/div/div/div[2]/form/div[2]/span/a";
+    public static final String WARNING_MESSAGE = "//*[@id=\"content\"]/div/div/div/div/div[2]/div";
+}
