48f921c20a75eba27c36e93a6d4449e79d7f71e3
Orest Zhmurkevych <orest.zhmurkevych@gmail.com>
2019-10-24 14:08:43 +0300

    Added pattern's path constants for a sikuli registration script

---
+++ src/main/java/patterns/RegistrationPatterns.java
@@ -0,0 +1,16 @@
+package patterns;
+
+public class RegistrationPatterns {
+
+    public static final String FIELD_FIRST_NAME = "src/main/resources/sikulipatterns/fieldFirstName.png";
+    public static final String FIELD_LAST_NAME = "src/main/resources/sikulipatterns/fieldLastName.png";
+    public static final String FIELD_EMAIL = "src/main/resources/sikulipatterns/fieldEmail.png";
+    public static final String FIELD_TELEPHONE = "src/main/resources/sikulipatterns/fieldTelephone.png";
+    public static final String LABEL_YOUR_PASSWORD = "src/main/resources/sikulipatterns/labelYourPassword.png";
+    public static final String CHECKBOX_PRIVACY_POLICY = "src/main/resources/sikulipatterns/checkboxPrivacyPolicy.png";
+    public static final String BUTTON_CONTINUE = "src/main/resources/sikulipatterns/buttonContinue.png";
+    public static final String MESSAGE_ON_SUCCESSFUL_REGISTRATION = "src/main/resources/sikulipatterns/"
+            + "messageOnSuccessfulRegistration.png";
+    public static final String BUTTON_LOGOUT = "src/main/resources/sikulipatterns/buttonLogout.png";
+    public static final String MESSAGE_ON_SUCCESSFUL_LOGOUT = "src/main/resources/sikulipatterns/messageOnSuccessfulLogout.png";
+}
