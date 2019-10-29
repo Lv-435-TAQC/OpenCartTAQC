f3f56ec9df3f30bb96ff4979c6866361811927f4
nhardzhalo <ngardzhalo@gmail.com>
2019-10-29 14:10:39 +0200

    add jbbc connector

---
+++ src/main/java/entity/DBConnector.java
@@ -0,0 +1,40 @@
+package entity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static utils.DBConstants.*;
+
+public class DBConnector {
+    private Connection connection;
+    private Statement statement;
+
+    public Boolean getConnectionMariaDB(){
+        try {
+            Class.forName(MARIA_DB_DRIVER);
+            connection = DriverManager.getConnection(MARIA_DB_URL, MARIA_DB_USER_NAME, MARIA_DB_PASSWORD);
+            statement = connection.createStatement();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        Boolean isConnected = true;
+        try {
+            isConnected = !connection.isClosed();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return isConnected;
+    }
+    public Boolean closeConnectionMariaDB(){
+        Boolean isClosed = true;
+        try {
+            connection.close();
+            isClosed = connection.isClosed();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return isClosed;
+    }
+}
