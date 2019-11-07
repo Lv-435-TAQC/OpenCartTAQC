1e7b7d0b7da9ec4ec22bbc0903c3f4b0ac8eb605
Orysia Benko <orysita.lviv@gmail.com>
2019-11-07 12:01:01 +0200

    want pull

--- src/main/java/pageobjects/CheckoutPaymentMethodPageObject.java
+++ src/main/java/pageobjects/CheckoutPaymentMethodPageObject.java
@@ -1,6 +1,5 @@
 package pageobjects;
 
-import locators.CheckoutConfirmOrderLocators;
 import locators.CheckoutPaymentMethodLocators;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,7 +10,6 @@ import pageelements.Button;
 import pageelements.Checkbox;
 import pageelements.Input;
 
-
 public class CheckoutPaymentMethodPageObject extends BasePageObject {
     private Input addCommentsAboutYourOrderField;
     private Button continueButton;
@@ -20,19 +18,16 @@ public class CheckoutPaymentMethodPageObject extends BasePageObject {
 
     public CheckoutPaymentMethodPageObject(WebDriver driver) {
         super(driver);
-
-        }
+    }
 
     public CheckoutConfirmOrderPageObject paymentMethodWithCommentsAboutYourOrder(String addCommentsAboutYourOrder) {
         WebElement explicitWait = (new WebDriverWait(driver, 10)).
                 until(ExpectedConditions.presenceOfElementLocated(By.xpath(CheckoutPaymentMethodLocators.CONTINUE_PAYMENT_METHOD)));
-
         this
                 .setCommentsAboutYourOrder(addCommentsAboutYourOrder)
                 .checkOnPrivacyPolicyCheckbox()
                 .clickContinueButton();
-        System.out.println("clickContinueButton");
-      return new CheckoutConfirmOrderPageObject(driver);
+        return new CheckoutConfirmOrderPageObject(driver);
     }
 
     public CheckoutConfirmOrderPageObject paymentMethodWithoutCommentsAboutYourOrder() {
@@ -40,8 +35,7 @@ public class CheckoutPaymentMethodPageObject extends BasePageObject {
                 .clickFlatShippingRateButton()
                 .checkOnPrivacyPolicyCheckbox()
                 .clickContinueButton();
-      return new CheckoutConfirmOrderPageObject(driver);
-
+        return new CheckoutConfirmOrderPageObject(driver);
     }
 
     public CheckoutPaymentMethodPageObject setCommentsAboutYourOrder(String addCommentsAboutYourOrder) {
@@ -52,7 +46,7 @@ public class CheckoutPaymentMethodPageObject extends BasePageObject {
 
     public CheckoutPaymentMethodPageObject clickFlatShippingRateButton() {
         WebElement explicitWait = (new WebDriverWait(driver, 10)).
-                until(ExpectedConditions.presenceOfElementLocated(By.xpath( CheckoutPaymentMethodLocators.TERMS_AND_CONDITIONS)));
+                until(ExpectedConditions.presenceOfElementLocated(By.xpath(CheckoutPaymentMethodLocators.TERMS_AND_CONDITIONS)));
 
         cashOnDelivery = new Button(this.driver, CheckoutPaymentMethodLocators.CASH_ON_DELIVERY);
         cashOnDelivery.click();
@@ -61,9 +55,17 @@ public class CheckoutPaymentMethodPageObject extends BasePageObject {
 
     public CheckoutConfirmOrderPageObject clickContinueButton() {
         continueButton = new Button(this.driver, CheckoutPaymentMethodLocators.CONTINUE_PAYMENT_METHOD);
-        continueButton.click();
+        try {
+            Thread.sleep(5000);
+            continueButton.click();
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return new CheckoutConfirmOrderPageObject(driver);
     }
+
     public CheckoutPaymentMethodPageObject checkOnPrivacyPolicyCheckbox() {
         termsAndConditions = new Checkbox(driver, CheckoutPaymentMethodLocators.TERMS_AND_CONDITIONS);
         termsAndConditions.clickOnCheckbox();
--- src/test/javatest/pageobjectstest/CheckoutPageObjectTest.java
+++ src/test/javatest/pageobjectstest/CheckoutPageObjectTest.java
@@ -1,20 +1,15 @@
 package javatest.pageobjectstest;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import pageelements.Button;
 import pageobjects.CheckoutBillingDetailsPageObject;
 import pageobjects.HomePageObject;
 import utils.Constants;
 
-import static locators.CheckoutConfirmOrderLocators.CONFIRM_ORDER;
 import static org.testng.Assert.assertEquals;
 import static utils.Constants.*;
 
@@ -33,7 +28,7 @@ public class CheckoutPageObjectTest {
      * <li>4. Enter username, password and Click Login Tab;
      * <li>5. Click on Home Tab;
      * <li>6. Click on Menu/Phones and PDAs Tab;
-     * <li>7. Add Palm Treo Pro Item to Shopping Cart;
+     * <li>7. Add iPhone Item to Shopping Cart;
      * <li>8. Click on Shopping Cart Tab;
      * <li>9. Click on Checkout
      * </ul>
@@ -50,7 +45,7 @@ public class CheckoutPageObjectTest {
         homePageObject.goToHomePage()
                 .getHeaderPageObject()
                 .clickLoginPage()
-                .logIn("orysita.lviv+1@gmail.com", "orysia");
+                .logIn("orysita.lviv+1000@gmail.com", "orysia");
     }
 
     @BeforeMethod
@@ -67,46 +62,10 @@ public class CheckoutPageObjectTest {
 
     }
 
-//    @AfterMethod
-//    public void logout() {
-//        checkoutBillingDetails.goToUrl(LOGOUT_URL);
-//        (new WebDriverWait(driver, 30))
-//                .until(ExpectedConditions.textToBe(By.xpath("//div[@id = 'content']/h1"), "Account Logout"));
-//    }
-
-//    @AfterClass
-//    public void closeUp() {
-//        driver.quit();
-//    }
-
-    /**
-     * <b>TC-1: Test checkout     </b>
-     *     IT IS LIST OF EVENT
-     * Scenario:
-     * <ul>
-     * <li> Click I want to use an existing address on Billing Details
-     * <li> Click I want to use a new address on Billing Details
-     * <li> Input all information about user on Billing Details
-     * <li> Click Continue Button on Billing Details
-     * <li> Click I want to use an existing address on Delivery Details
-     * <li> Click I want to use a new address on Delivery Details
-     * <li> Click Continue Button on Delivery Details
-     * <li> Click Flat Rate on Delivery Method
-     * <li> Add comments about your order on Delivery Method
-     * <li> Click Continue Button on Delivery Method
-     * <li> Click "Cash on Delivery" on Payment Method
-     * <li> Add comments about your order on Payment Method
-     * <li> Click "I have read and agree to the Terms & Conditions" on Payment Method
-     * <li> Click Continue Button on Payment Method
-     * <li> Click Confirm Order on Confirm Order
-     * <li> Click OK on alert
-     * <li> Click Confirm Order on Confirm Order
-     *
-     * </ul>
-     * <p>
-     * Expected Result:
-     */
-
+    @AfterClass
+    public void closeUp() {
+        driver.quit();
+    }
 
     /**
      * <b>TC-1: Test checkout </b>
@@ -132,6 +91,7 @@ public class CheckoutPageObjectTest {
      * <p>
      * Expected Result: Your order has been placed!
      */
+
     @Test(priority = 1)
     public void placeAnOrderWithYourCurrentAddressAndAllComments() {
         String actual = checkoutBillingDetails
@@ -177,9 +137,35 @@ public class CheckoutPageObjectTest {
         assertEquals(SUCCSSES_ORDER1.equalsIgnoreCase(actual) ? Constants.SUCCSSES_ORDER1 : Constants.SUCCSSES_ORDER2, actual);
     }
 
+    /**
+     * <b>TC-2: Test checkout </b>
+     * <p>
+     * Scenario:
+     * <ul>
+     * <li> 1. Click I want to use a new address on Billing Details
+     * <li> 2.  Input all information about user on Billing Details
+     * <li> 3. Click Continue Button on Billing Details
+     * <li> 4. Click I want to use a new address on Delivery Details
+     * <li> 5. Input all information about user on Delivery Details
+     * <li> 6. Click Continue Button on Delivery Details
+     * <li> 7. Click Flat Rate on Delivery Method
+     * <li> 8. Add comments about your order on Delivery Method
+     * <li> 9. Click Continue Button on Delivery Method
+     * <li> 10. Click "Cash on Delivery" on Payment Method
+     * <li> 11. Add comments about your order on Payment Method
+     * <li> 12. Click "I have read and agree to the Terms & Conditions" on Payment Method
+     * <li> 13. Click Continue Button on Payment Method
+     * <li> 14. Click Confirm Order on Confirm Order
+     * <li> 15. Click OK on alert
+     * <li> 16. Click Confirm Order on Confirm Order
+     *
+     * </ul>
+     * <p>
+     * Expected Result: Your order has been placed!
+     */
 
     @Test(priority = 3)
-    public void placeAnOrderWithYourNewAddressAndNotAllCommentsAndAllInformationTest() {
+    public void placeAnOrderWithYourNewAddressAndAllCommentsAndAllInformationTest() {
         String actual =
                 checkoutBillingDetails
                         .continueWantUseNewAddressButtonWithAllInformation(
@@ -188,42 +174,9 @@ public class CheckoutPageObjectTest {
                                 "125463", "Ukraine", 3493)
                         .continueWantUseNewAddressButtonWithAllInformation("Orysia", "Benko", "SoftServe",
                                 "Chervonoy Kalyny 51", "Suxiv", "Lviv", "125463", "Ukraine", 3490)
-                        .deliveryMethodWithoutCommentsAboutYourOrder()
-                        .paymentMethodWithoutCommentsAboutYourOrder()
+                        .deliveryMethodWithCommentsAboutYourOrder("buy")
+                        .paymentMethodWithCommentsAboutYourOrder("paid")
                         .clickContinueButtonX().successMessage();
         assertEquals(SUCCSSES_ORDER1.equalsIgnoreCase(actual) ? Constants.SUCCSSES_ORDER1 : Constants.SUCCSSES_ORDER2, actual);
     }
-
-    @Test(priority = 4)
-    public void placeAnOrderWithYourNewAddressAndAllCommentsAndInformationTest() {
-//        String actual =
-        checkoutBillingDetails
-                .continueWantUseNewAddressButtonWithAllInformation("Orysia", "Benko", "SoftServe",
-                        "Chervonoy Kalyny 51", "Suxiv", "Lviv",
-                        "125463", "Thailand", 3192)
-                .continueWantUseNewAddressButtonWithAllInformation("Orysia", "Benko", "SoftServe",
-                        "Chervonoy Kalyny 51", "Suxiv", "Lviv",
-                        "125463", "Thailand", 3192)
-                .deliveryMethodWithCommentsAboutYourOrder("buy")
-                .paymentMethodWithCommentsAboutYourOrder("paid");
-//        driver.findElement(By.xpath("//a[@class = 'accordion-toggle collapsed' and @href = '#collapse-checkout-confirm']")).click();
-        WebDriverWait wait = (new WebDriverWait(driver, 20));
-        Boolean result = wait.until(ExpectedConditions.textToBe
-                (By.xpath("/html/body/div[2]/div/div/div/div[6]/div[2]/div/div[1]/table/tfoot/tr[2]/td[1]/strong"), "Flat Shipping Rate:"));
-        if (result) {
-            driver.findElement(By.xpath("//input[@type = 'button' and @value = 'Confirm Order']")).click();
-        }
-        (new WebDriverWait(driver, 20)).until(ExpectedConditions.alertIsPresent());
-        if (driver.switchTo().alert() != null) {
-            driver.switchTo().alert().accept();
-        } else {
-            new Button(this.driver, CONFIRM_ORDER).click();
-        }
-//        assertEquals(SUCCSSES_ORDER1.equalsIgnoreCase(actual) ? Constants.SUCCSSES_ORDER1 : Constants.SUCCSSES_ORDER2, actual);
-    }
-
-    @Test
-    public void placeAnOrderWithYourNewAddressAndNotAllCommentsAndNotAllInformationTest() {
-    }
-
 }
