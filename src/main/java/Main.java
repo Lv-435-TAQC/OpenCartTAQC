5e72206ca04bd58974cba21b92edd1dc5049654c
nhardzhalo <ngardzhalo@gmail.com>
2019-11-07 13:42:24 +0200

    fix main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,6 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import pageobjects.AdminPageObject;
-
 public class Main {
 
     public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
-        AdminPageObject adminPageObject = new AdminPageObject(driver);
-
 
     }
 }
