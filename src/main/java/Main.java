a02b3474eee00877f084547625763e05d85f964e
andr0306 <andrijrubchuk@gmail.com>
2019-11-19 14:02:37 +0200

    add structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import greencity.Security;
 import utils.DBMethods;
 
 public class Main {
 
     public static void main(String[] args) {
+        Security sec = new Security();
+        System.out.println(sec.signIn("oleh.zarichnyi@gmail.com","QWErty123$%^"));
     }
 }
