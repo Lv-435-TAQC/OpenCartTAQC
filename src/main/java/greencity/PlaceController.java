255c3031c477195b18b9ea8184b4c6439bf8daac
andr0306 <andrijrubchuk@gmail.com>
2019-11-25 13:28:46 +0200

    fix

---
+++ src/main/java/greencity/PlaceController.java
@@ -0,0 +1,42 @@
+package greencity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static utils.GreenCityURL.LOGIN_URL;
+
+public class PlaceController {
+
+    public String getPlaceById(int id) {
+        Map<String, String> map = new HashMap();
+        map.put("Content-Type", "application/json;utf-8");
+        BaseHttpRequest baseHttpRequest = new BaseHttpRequest();
+        baseHttpRequest.getRequest("https://greencitysoftserve.herokuapp.com//place/about/" + id);
+        return baseHttpRequest.getResponseJsonObject().toString();
+    }
+
+
+    public String getStatuses() {
+        Map<String, String> map = new HashMap();
+        map.put("Content-Type", "application/json;utf-8");
+        BaseHttpRequest baseHttpRequest = new BaseHttpRequest();
+        baseHttpRequest.getRequest("https://greencitysoftserve.herokuapp.com/place/statuses");
+        return baseHttpRequest.getResponseJsonArray().toString();
+    }
+
+    public String saveFavoritePlace(String token) {
+        Map<String, String> map = new HashMap();
+        map.put("Content-Type", "application/json;utf-8");
+        map.put("Authorization", "Bearer " + token);
+        BaseHttpRequest baseHttpRequest = new BaseHttpRequest();
+        baseHttpRequest.setRequestHeaders(map);
+        baseHttpRequest.postRequest("https://greencitysoftserve.herokuapp.com/place/save/favorite/",  "{\n" +
+                "  \"name\": \"HHhsdwea\",\n" +
+                "  \"placeId\": 2\n" +
+                "}");
+        return baseHttpRequest.getResponseJsonObject().toString();
+    }
+}
+
+
+
